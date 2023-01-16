a4fde53cc2ad66cfcb02573ad5680d6ccafbf89c
ssilatel <setaxe25@gmail.com>
2023-01-16 08:40:52 +0100

    Refactoring to OOP

--- port-scanner.py
+++ port-scanner.py
@@ -1,74 +1,71 @@
 #!/bin/python3
-
+import argparse
 import socket
 import sys
 
-def print_help_message():
-    print("""
-    Usage: ./port-scanner [HOSTNAME] [PORT]
+class CLIArguments:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description="Scan any number of ports on a target machine")
+        self.parser.add_argument("-t", "--target", help="Target machine to scan")
+        self.p = self.parser.add_mutually_exclusive_group()
+        self.p.add_argument("-a", "--all", help="Scan all ports", action="store_true")
+        self.p.add_argument("-p", "--ports", help="Specify ports (separated by a comma if multiple)")
+        self.args = vars(self.parser.parse_args())
 
-    [PORT]: specify a single port, multiple ports separated by commas or 'a' for all ports
-    """)
-    sys.exit()
+    def get_args(self):
+        return self.args
 
-def handle_arguments():
-    if len(sys.argv) == 2 and (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
-        print_help_message()
-    elif len(sys.argv) != 3:
-        print_help_message()
-    else:
-        scan_ports()
+class PortScanner:
+    def __init__(self, args):
+        self.target = args["target"]
+        self.all = args["all"]
+        self.ports = args["ports"]
 
-def scan_all_ports():
-    HOST = sys.argv[1]
-    found_open_ports = False
-    for i in range(1, 65536):
+    def scan_all_ports(self):
+        found_open_ports = False
+        for i in range(1, 65536):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((self.target, i))
+            if result == 0:
+                print(f"Port {i} on {self.target} is open.")
+                found_open_ports = True
+        if not found_open_ports:
+            print(f"Could not detect any open ports on {self.target}")
+        sock.close()
+
+    def scan_multiple_ports(self):
+        ports_list = self.ports.split(",")
+        found_open_ports = False
+        for p in ports_list:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((self.target, int(p)))
+            if result == 0:
+                print(f"Port {p} on {self.target} is open.")
+                found_open_ports = True
+        if not found_open_ports:
+            print(f"None of the specified ports are open on {self.target}")
+        sock.close()
+
+    def scan_single_port(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((HOST, i))
+        result = sock.connect_ex((self.target, int(self.ports)))
         if result == 0:
-            print(f"Port {i} on {HOST} is open.")
-            found_open_ports = True
-    if not found_open_ports:
-        print(f"Could not detect any open ports on {HOST}")
-    sock.close()
-
-def scan_multiple_ports():
-    HOST = sys.argv[1]
-    PORTS = sys.argv[2]
-    ports_list = PORTS.split(",")
-    found_open_ports = False
-    for p in ports_list:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((HOST, int(p)))
-        if result == 0:
-            print(f"Port {p} on {HOST} is open.")
-            found_open_ports = True
-    if not found_open_ports:
-        print(f"None of the specified ports are open on {HOST}")
-    sock.close()
-
-def scan_single_port():
-    HOST = sys.argv[1]
-    PORT = int(sys.argv[2])
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((HOST, PORT))
-    if result == 0:
-        print(f"Port {PORT} on {HOST} is open.")
-    else:
-        print(f"Port {PORT} on {HOST} is closed.")
-    sock.close()
-
-
-def scan_ports():
-    if sys.argv[2] == "-a":
-        scan_all_ports()
-    else:
-        PORTS = sys.argv[2]
-        if "," in PORTS:
-            scan_multiple_ports()
+            print(f"Port {self.ports} on {self.target} is open.")
         else:
-            scan_single_port()
+            print(f"Port {self.ports} on {self.target} is closed.")
+        sock.close()
+
+
+    def scan_ports(self):
+        if self.all:
+            self.scan_all_ports()
+        else:
+            if "," in self.ports:
+                self.scan_multiple_ports()
+            else:
+                self.scan_single_port()
 
 if __name__ == "__main__":
-    handle_arguments()
+    clia = CLIArguments()
+    ps = PortScanner(clia.get_args())
+    ps.scan_ports()
