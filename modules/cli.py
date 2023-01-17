c14c5ff93378be242ef8401c5ccb71715797a624
eonraider <livewire_voodoo@protonmail.com>
2023-01-17 11:19:26 -0300

    Remove nested function

--- modules/cli.py
+++ modules/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 from typing import Any, Generator
 
 
@@ -22,22 +23,20 @@ class CLIArgumentsParser:
             help="Specify ports (separated by a comma if multiple)"
         )
         self.parsed_args = self.parser.parse_args(*args, **kwargs)
-        self._process_port_ranges()
+        self.parsed_args.ports = tuple(self._process_port_ranges())
         return self.parsed_args
 
-    def _process_port_ranges(self) -> None:
+    def _process_port_ranges(self) -> Generator[int, Any, None]:
         """Yield an iterator with integers extracted from a string
         consisting of mixed port numbers and/or ranged intervals.
         Ex: From '20-25,53,80,111' to (20,21,22,23,24,25,53,80,111)
         """
-        def _parse_ports() -> Generator[int, Any, None]:
-            for port in self.parsed_args.ports.split(','):
-                try:
-                    port = int(port.strip())
-                    if not 0 < port < 65536:
-                        raise SystemExit(f'Error: Invalid port number {port}.')
-                    yield port
-                except ValueError:
-                    start, end = (int(port) for port in port.split('-'))
-                    yield from range(start, end + 1)
-        self.parsed_args.ports = tuple(_parse_ports())
+        for port in re.split(r"\s*,\s*", self.parsed_args.ports):
+            try:
+                port = int(port)
+                if not 0 < port < 65536:
+                    raise SystemExit(f'Error: Invalid port number {port}.')
+                yield port
+            except ValueError:
+                start, end = (int(port) for port in port.split('-'))
+                yield from range(start, end + 1)
