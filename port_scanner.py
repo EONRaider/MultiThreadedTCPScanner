597452ce5d2cd90e1e85cd2ac73657c4b2c6dc1f
eonraider <livewire_voodoo@protonmail.com>
2023-01-17 16:16:12 -0300

    Implement command pattern

--- port_scanner.py
+++ port_scanner.py
@@ -1,64 +1,23 @@
 #!/usr/bin/python3
-import socket
-from collections.abc import Collection
-from dataclasses import dataclass
-from enum import Enum
-from typing import Iterator
-
 from modules.cli import CLIArgumentsParser
-from modules.exceptions import HostnameResolutionError
+from modules.tcp_connect import TCPConnectScanner
 
 
-class PortState(Enum):
-    UNDEFINED = "Undefined"
-    OPEN = "Open"
-    TIMEOUT = "Closed | Timeout"
-    CONNREFUSED = "Closed | ConnectionRefused"
+class PortScanner:
+    def __init__(self):
+        self.cli_args = CLIArgumentsParser().parse()
+        self.tcp_connect = TCPConnectScanner(
+            target=self.cli_args.target,
+            ports=self.cli_args.ports,
+            timeout=self.cli_args.timeout)
 
-
-@dataclass
-class ScanResult:
-    port: int
-    state: PortState = PortState.UNDEFINED
-
-
-class TCPConnectScanner:
-    def __init__(self, target: str, ports: Collection[int], timeout: int):
-        self.target = target
-        self.ports = ports
-        self.timeout = timeout
-        self.results: list[ScanResult] = []
-
-    def execute(self) -> Iterator[ScanResult]:
-        for port in self.ports:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.settimeout(self.timeout)
-                try:
-                    result = ScanResult(port)
-                    sock.connect((self.target, port))
-                except socket.gaierror:
-                    yield HostnameResolutionError(
-                        f"Failed to connect or resolve hostname to target "
-                        f"address {self.target}"
-                    )
-                except socket.timeout:
-                    result.state = PortState.TIMEOUT
-                except ConnectionRefusedError:
-                    result.state = PortState.CONNREFUSED
-                else:
-                    result.state = PortState.OPEN
-                self.results.append(result)
-            yield result
+    def execute(self) -> None:
+        try:
+            for scan_result in self.tcp_connect.execute():
+                print(scan_result)
+        except KeyboardInterrupt:
+            raise SystemExit("[!] Aborting port scanner...")
 
 
 if __name__ == "__main__":
-    cli_args = CLIArgumentsParser().parse()
-    scanner = TCPConnectScanner(
-        target=cli_args.target,
-        ports=cli_args.ports,
-        timeout=cli_args.timeout)
-    try:
-        for scan_result in scanner.execute():
-            print(scan_result)
-    except KeyboardInterrupt:
-        raise SystemExit("Aborting port scanner...")
+    PortScanner().execute()
