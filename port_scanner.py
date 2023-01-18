6b364f37f076162040d736cfe63a3004dc8f2ddf
eonraider <livewire_voodoo@protonmail.com>
2023-01-18 10:21:38 -0300

    Implement observer pattern

--- port_scanner.py
+++ port_scanner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from modules.cli import CLIArgumentsParser
+from modules.exceptions import PortScannerException
+from modules.output.screen import ScreenOutput
 from modules.tcp_connect import TCPConnectScanner
 
 
@@ -9,12 +11,15 @@ class PortScanner:
         self.tcp_connect = TCPConnectScanner(
             target=self.cli_args.target,
             ports=self.cli_args.ports,
-            timeout=self.cli_args.timeout)
+            timeout=self.cli_args.timeout,
+        )
 
     def execute(self) -> None:
+        ScreenOutput(scanner=self.tcp_connect)
         try:
-            for scan_result in self.tcp_connect.execute():
-                print(scan_result)
+            for result in self.tcp_connect.execute():
+                if isinstance(result, PortScannerException):
+                    raise result
         except KeyboardInterrupt:
             raise SystemExit("[!] Aborting port scanner...")
 
