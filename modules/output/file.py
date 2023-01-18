71ce153bc67c88ad01ea74d97864a5f1465a3918
eonraider <livewire_voodoo@protonmail.com>
2023-01-18 11:30:39 -0300

    Add file.py

---
+++ modules/output/file.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from modules.core import ScanResult
+from modules.output.base import OutputProcessor
+
+
+class FileOutput(OutputProcessor):
+    def __init__(self, scanner, path: [str, Path]):
+        super().__init__(scanner)
+        self.file = Path(path).open("a")
+
+    def __del__(self):
+        self.file.close()
+
+    def update(self, result: ScanResult) -> None:
+        self.file.write(f"{result.port}, {result.state.value}\n")
