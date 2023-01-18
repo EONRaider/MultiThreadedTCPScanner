ff0574eb2ed6a732651d3b65e6b36ad4d57a3636
eonraider <livewire_voodoo@protonmail.com>
2023-01-18 11:38:30 -0300

    Modify docstring

--- modules/output/base_processor.py
+++ modules/output/base_processor.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class OutputProcessor(ABC):
     """Interface for the implementation of all classes responsible for
     further processing/output of the information gathered by the
-    PortScanner class."""
+    TCPConnectScanner class."""
 
     def __init__(self, scanner):
         self.scanner = scanner
