ded3795c05b8c7236f07edbf8c839f3fd8bcb398
eonraider <livewire_voodoo@protonmail.com>
2023-01-18 12:20:15 -0300

    Add prompt for file write operation

--- modules/output/file.py
+++ modules/output/file.py
@@ -7,10 +7,12 @@ from modules.output.base_processor import OutputProcessor
 class FileOutput(OutputProcessor):
     def __init__(self, scanner, path: [str, Path]):
         super().__init__(scanner)
-        self.file = Path(path).open("a")
+        self.path = Path(path)
+        self.file = self.path.open("a")
 
     def __del__(self):
         self.file.close()
+        print(f"[+] Scan results successfully written to {self.path}")
 
     def update(self, result: ScanResult) -> None:
         self.file.write(f"{result.port}, {result.state.value}\n")
