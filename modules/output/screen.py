d2cab510e61db1d308f1f97bf8052f0bd079508d
eonraider <livewire_voodoo@protonmail.com>
2023-01-18 12:20:39 -0300

    Add prompts for time measurement operations

--- modules/output/screen.py
+++ modules/output/screen.py
@@ -1,3 +1,5 @@
+from time import ctime, time
+
 from modules.core import ScanResult
 from modules.output.base_processor import OutputProcessor
 
@@ -7,7 +9,15 @@ class ScreenOutput(OutputProcessor):
         super().__init__(scanner)
         self._initialize()
 
+    def __del__(self):
+        print(
+            f"[+] TCP-connect scan of {self.scanner.num_ports} ports for "
+            f"{self.scanner.target} completed in {self.scanner.total_time:.2f} "
+            f"seconds"
+        )
+
     def _initialize(self) -> None:
+        print(f"[+] Starting port scanner at {ctime(time())}")
         print(f"[+] Scan results for {self.scanner.target}")
 
     @staticmethod
