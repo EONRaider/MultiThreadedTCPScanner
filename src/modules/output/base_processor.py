46df2a9218c29b00808aa2328d479b132331b5d4
eonraider <livewire_voodoo@protonmail.com>
2023-01-20 20:40:10 -0300

    Add docstrings

--- src/modules/output/base_processor.py
+++ src/modules/output/base_processor.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class OutputProcessor(ABC):
     """Interface for the implementation of all classes responsible for
     further processing/output of the information gathered by the
-    TCPConnectScanner class."""
+    TCPConnect class."""
 
     def __init__(self, scanner):
         self.scanner = scanner
@@ -12,10 +12,21 @@ class OutputProcessor(ABC):
 
     @abstractmethod
     def update(self, *args, **kwargs) -> None:
+        """
+        Interface for the execution of a main operation by the observer.
+        """
         ...
 
     def startup(self, *args, **kwargs) -> None:
+        """
+        Performs any operations deemed necessary prior to the first call
+        to the update method.
+        """
         ...
 
     def cleanup(self, *args, **kwargs) -> None:
+        """
+        Performs any operations deemed necessary after to the last call
+        to the update method.
+        """
         ...
