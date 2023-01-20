fae3df8395dbd6278e5e22abfb307a1c25ecdc14
eonraider <livewire_voodoo@protonmail.com>
2023-01-20 12:50:41 -0300

    Modify tests

--- tests/unit/test_scan_result.py
+++ tests/unit/test_scan_result.py
@@ -10,5 +10,11 @@ class TestScanResult:
 
     def test_state_change(self):
         result = ScanResult(80)
+        result.state = PortState.OPEN
+        assert result.state.value == "Open | SYN / ACK"
         result.state = PortState.TIMEOUT
         assert result.state.value == "Closed | No Response"
+        result.state = PortState.CONNREFUSED
+        assert result.state.value == "Closed | Connection Refused"
+        result.state = PortState.NETERROR
+        assert result.state.value == "Network Error"
