4232d7253db5952e24486eca5f68b544a43ab5a9
eonraider <livewire_voodoo@protonmail.com>
2023-01-20 20:29:11 -0300

    Add docstrings

--- src/port_scanner.py
+++ src/port_scanner.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from src.modules.exceptions import PortScannerException
 from src.modules.output.file import FileOutput
 from src.modules.output.screen import ScreenOutput
-from src.modules.tcp_connect import TCPConnect
+from src.modules.scan_modes.tcp_connect import TCPConnect
 
 
 class PortScanner:
@@ -17,6 +17,17 @@ class PortScanner:
         timeout: float,
         output_file_path: [str, Path] = None,
     ):
+        """
+        A single-threaded TCP-connect port scanner
+
+        :param target: Hostname of the target to be scanned.
+        :param ports: A comma-separated list of ports and/or port ranges
+            to scan.
+        :param timeout: Time in seconds to wait for a host's response
+            before giving up and considering the port as closed.
+        :param output_file_path: Absolute path to a file in which scan
+            results will be saved.
+        """
         self.target = target
         self.ports = ports
         self.timeout = timeout
@@ -28,6 +39,9 @@ class PortScanner:
         )
 
     def execute(self) -> None:
+        """
+        Run a port scanning operation in TCP-connect mode.
+        """
         ScreenOutput(scanner=self.tcp_connect)
         if self.file_path is not None:
             FileOutput(scanner=self.tcp_connect, path=self.file_path)
