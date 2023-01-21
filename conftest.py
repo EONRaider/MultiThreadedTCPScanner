26d14415722280d059cbc9012d769f8c98091ac6
eonraider <livewire_voodoo@protonmail.com>
2023-01-21 10:03:33 -0300

    Add pytest configuration files

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.modules.scan_modes.tcp_connect import TCPConnect
+
+
+@pytest.fixture
+def mock_scanner():
+    return TCPConnect(
+        target="non_existent_host_9328jdjks7887s.com",
+        ports=[21, 53, 80, 443],
+        timeout=1,
+    )
